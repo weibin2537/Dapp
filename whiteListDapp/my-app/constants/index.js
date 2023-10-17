a78346df6fca5ced526b0777d0bf681ff9f2e3c5
weibin2537 <2537444533@qq.com>
2023-10-17 09:56:34 +0800

    Dapp used the sepolia testnet to create the whiteList

---
+++ whiteListDapp/my-app/constants/index.js
@@ -0,0 +1,37 @@
+export const abi = [
+    {
+      "inputs": [
+        {
+          "internalType": "uint8",
+          "name": "_maxWhitelistedAddresses",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [],
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "whitelistedAddresses",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ];
+export const WHITELIST_CONTRACT_ADDRESS = "0x4dA7b02962fb48E3EF2E57d06D5B00Efc6D62ADB";
+
